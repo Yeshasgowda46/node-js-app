94da67bcf9e8727652607225079c1095ecbd3202
Yashas DS <yeshasgowda46@gmail.com>
2026-03-18 15:27:12 +0530

    Initial node app

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Yashas');
+  res.send('Hello Yashas the change is fixed');
 });
 
 app.listen(3000, () => {
